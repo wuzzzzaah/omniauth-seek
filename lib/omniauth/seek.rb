542d4eae711b45058df6dacb5c321fec861e04c5
Omar Shariff <omarshariffd@gmail.com>
2017-06-23 18:10:54 +0800

    Initial commit

---
+++ lib/omniauth/seek.rb
@@ -0,0 +1,7 @@
+require "omniauth/seek/version"
+
+module Omniauth
+  module Seek
+    # Your code goes here...
+  end
+end
---
+++ spec/omniauth/seek_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Omniauth::Seek do
+  it "has a version number" do
+    expect(Omniauth::Seek::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
